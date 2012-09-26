1d7bcf902073e119d4541cef6d0a029f52aab945
RobertBorg <rauban@gmail.com>
2012-09-26 17:25:57 +0200

    implemented Database wrapper structure

---
+++ src/model/database.php
@@ -0,0 +1,15 @@
+<?php
+define("DB_USER", "db_username");
+define("DB_PASSWORD", "db_password");
+define("DB_DATABASE", "db_database");
+
+define("USER", "User");
+define("PRODUCT", "Product");
+define("SESSION", "Session");
+class Database {
+	public function create($object, $type) {
+	}
+	public function read($id, $type) {
+	}
+}
+?>
