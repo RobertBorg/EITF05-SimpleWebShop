ea6047df6785779b08d8625ceb2f3a6a6d9d5ca4
RobertBorg <rauban@gmail.com>
2012-09-26 22:54:50 +0200

    added mysql connection to Database

--- src/model/database.php
+++ src/model/database.php
@@ -1,12 +1,26 @@
 <?php
+define("DB_HOST", "localhost");
 define("DB_USER", "db_username");
 define("DB_PASSWORD", "db_password");
-define("DB_DATABASE", "db_database");
+define("DB_DATABASE", "SimpleWebShop");
 
 define("USER", "User");
 define("PRODUCT", "Product");
 define("SESSION", "Session");
 class Database {
+	private $mysql_conn;
+	public function __construct() {
+		connect();
+	}
+	
+	private function connect() {
+		$this->mysql_conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD , DB_DATABASE);
+		if (mysqli_connect_errno()) {
+			printf("Connect failed: %s\n", mysqli_connect_error());
+			exit();
+		}
+	}
+	
 	public function create($object, $type) {
 		switch ($type) {
 		case USER:
