0666a137423f83bc578ea08d3e2dd1b5d3eef606
RobertBorg <rauban@gmail.com>
2012-10-01 17:56:03 +0200

    changed order of members, added constructor to create from database

--- src/model/user.php
+++ src/model/user.php
@@ -1,8 +1,14 @@
 <?php
 class User {
 	private $m_username;
-	private $m_password_hashed;
 	private $m_home_address;
+	private $m_password_hashed;
 	private $m_password_salt;
+	public function __construct($username, $home_address, $password_hashed, $password_salt) {
+		$this->m_username = $username;
+		$this->m_home_address = $home_address;
+		$this->m_password_hashed = $password_hashed;
+		$this->m_password_salt = $password_salt;
+	}
 }
 ?>
