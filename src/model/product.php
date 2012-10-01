75d733c1c87e41f8431404c8a547a1944651f853
RobertBorg <rauban@gmail.com>
2012-10-01 17:56:16 +0200

    added constructor to create from raw data

--- src/model/product.php
+++ src/model/product.php
@@ -3,5 +3,10 @@ class Product {
 	private $m_product_id;
 	private $m_name;
 	private $m_price;
+	public function __construct($product_id, $name, $price) {
+		$this->m_product_id = $product_id;
+		$this->m_name = $name;
+		$this->m_price = $price;
+	}
 }
 ?>
